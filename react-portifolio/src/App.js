64e8fc04fa29106cfa26d082184c4b1b3491d611
RosaUlisses <bolissesrosa@gmail.com>
2022-06-24 09:11:16 -0300

    first commit

---
+++ react-portifolio/src/App.js
@@ -0,0 +1,13 @@
+import './Styles/App.css'
+import './Components/header'
+import {_header} from './Components/header'
+
+function App() {
+  return (
+    <div>
+      <_header/>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-portifolio/src/Components/header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import '../Styles/header.css'
+
+import {Button, Badge} from 'react-bootstrap';
+
+export const _header = () => {
+    return(
+        <div>
+        </div>
+    );
+}
